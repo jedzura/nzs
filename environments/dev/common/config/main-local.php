df64d603642a99787fd5c3e52148d048ca13ee05
jedzura <jedzura@gmail.com>
2015-10-14 23:06:06 +0200

    Db config

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=nzs',
             'username' => 'root',
-            'password' => '',
+            'password' => 'alfa155',
             'charset' => 'utf8',
         ],
         'mailer' => [
